f117a4620f038046da5678cd167897294ce3aa3f
Alex Lapp <watertower5132@gmail.com>
2020-09-23 15:10:22 -0500

    Create custom keymap for atreus62 rgb

--- keyboards/atreus62rgb/keymaps/default/keymap.c
+++ keyboards/atreus62rgb/keymaps/default/keymap.c
@@ -10,42 +10,42 @@
 // entirely and just use numbers.
 #define _DEFAULT 0
 #define _SYMBOLS 1
-#define _MEDIA 2
-#define _GAME 3
+#define _GAME 2
+#define _MEDIA 3
 #define _RESET 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEFAULT] = LAYOUT( /* qwerty */
-    KC_EQUAL,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,     KC_8,    KC_9,    KC_0,                KC_MINS ,
-    KC_GRAVE,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,                KC_BSLS ,
-    MO(_SYMBOLS),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,     KC_K,    KC_L,    LT(_MEDIA, KC_SCLN), KC_QUOT ,
-    KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH,             KC_RSHIFT ,
-    KC_LCTL,     KC_LGUI, KC_LALT, KC_GRV,  KC_LCTL, KC_BSPC, KC_TAB,   KC_ENT,   KC_SPC,  KC_RCTL,  KC_MINS, KC_QUOT, KC_ENT,              KC_RGUI
-  ),
-  
-  [_SYMBOLS] = LAYOUT(
-    KC_TRNS,  KC_F1,      KC_F2,   KC_F3,       KC_F4,       KC_F5,                       KC_F6,         KC_F7,         KC_F8,         KC_F9,         KC_F10,  KC_F11  ,
-    KC_TRNS,  KC_TRNS,    KC_TRNS, KC_LCBR,     KC_RCBR,     KC_TRNS,                     KC_F12,        KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS,    KC_TRNS, KC_LPRN,     KC_RPRN,     KC_TRNS,                     KC_LEFT,       KC_DOWN,       KC_UP  ,       KC_RGHT,       KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS,    KC_TRNS, KC_LBRACKET, KC_RBRACKET, KC_TRNS,                     LSFT(KC_LEFT), LSFT(KC_DOWN), LSFT(KC_UP)  , LSFT(KC_RGHT), KC_TRNS, KC_TRNS ,
-    KC_TRNS,  TO(_RESET), KC_TRNS, KC_TRNS,     KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS, TO(_GAME)
+    KC_EQUAL,  KC_1,               KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,     KC_8,        KC_9,        KC_0,                KC_MINS ,
+    KC_TAB,    KC_Q,               KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,     KC_I,        KC_O,        KC_P,                KC_BSLS ,
+    KC_LCTL,   KC_A,               KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,     KC_K,        KC_L,        KC_SCLN,             LT(_MEDIA, KC_QUOT),
+    KC_LSFT,   LT(_SYMBOLS, KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,     KC_COMM,     KC_DOT,      LT(_GAME, KC_SLSH),  KC_RSHIFT ,
+    KC_ESC,    KC_LGUI,            KC_NO,   KC_GRV,  KC_LCTL, KC_BSPC, KC_LALT,   KC_ENT,   KC_SPC,  KC_RCTL,  KC_LBRACKET, KC_RBRACKET, KC_HOME,             KC_END
   ),
 
-  
+  [_SYMBOLS] = LAYOUT(
+    KC_TRNS,  KC_F1,      KC_F2,   KC_F3,       KC_F4,       KC_F5,                         KC_F6,         KC_F7,      KC_F8,      KC_F9,      KC_F10,  KC_F11  ,
+    KC_TRNS,  KC_TRNS,    KC_TRNS, KC_LCBR,     KC_RCBR,     KC_PIPE,                       KC_F12,        KC_7,       KC_8,       KC_9,       KC_TRNS, KC_F12  ,
+    KC_TRNS,  KC_TRNS,    KC_TRNS, KC_LPRN,     KC_RPRN,     KC_GRAVE,                      KC_TRNS,       KC_4,       KC_5,       KC_6,       KC_TRNS, KC_TRNS ,
+    KC_TRNS,  KC_TRNS,    KC_TRNS, KC_LBRACKET, KC_RBRACKET, KC_TILDE,                      KC_TRNS,       KC_1,       KC_2,       KC_3,       KC_TRNS, KC_TRNS ,
+    KC_TRNS,  TO(_RESET), KC_TRNS, KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_0,       KC_0,       KC_DOT,     KC_TRNS, TO(_GAME)
+  ),
+
+  [_GAME] = LAYOUT(
+    KC_ESC ,  KC_1   , KC_2   , KC_3   , KC_4   , KC_5    ,                    KC_6    , KC_7   , KC_8  ,  KC_9    , KC_0    , KC_NO     ,
+    KC_NO  ,  KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R    ,                    KC_NO   , KC_NO  , KC_UP  , KC_NO   , KC_NO   , KC_NO     ,
+    KC_NO  ,  KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F    ,                    KC_NO   , KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO   , MO(_MEDIA),
+    KC_NO  ,  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V    ,                    KC_NO   , KC_NO  , KC_NO  , KC_NO   , KC_TRNS , KC_NO     ,
+    KC_M   ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_SPC  , KC_P  ,  KC_ENT,   KC_SPC  , KC_NO  , KC_NO  , KC_NO   , KC_NO   , TO(_DEFAULT)
+  ),
+
+
   [_MEDIA] = LAYOUT(
     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS,             KC_TRNS,             KC_TRNS,       KC_TRNS, KC_TRNS ,
     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_TRNS,       KC_TRNS, KC_TRNS ,
     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_AUDIO_VOL_DOWN,   KC_AUDIO_VOL_UP,     KC_AUDIO_MUTE, KC_TRNS, KC_TRNS ,
     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS,             KC_MEDIA_PLAY_PAUSE, KC_TRNS,       KC_TRNS, KC_TRNS ,
     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,             KC_TRNS,             KC_TRNS,       KC_TRNS, KC_TRNS
-  ),
-
-  [_GAME] = LAYOUT(
-    KC_ESC ,  KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO   , KC_NO  , KC_NO     ,
-    KC_NO  ,  KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   ,                     KC_NO  , KC_NO  , KC_UP  , KC_NO   , KC_NO  , KC_NO     ,
-    KC_NO  ,  KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   ,                     KC_NO  , KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO  , MO(_MEDIA),
-    KC_NO  ,  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO   , KC_NO  , KC_NO     ,
-    KC_M   ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_SPACE, KC_NO  , KC_NO  , KC_NO  , KC_NO   , KC_NO  , TO(_DEFAULT)
   ),
 
   [_RESET] = LAYOUT(
